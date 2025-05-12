cd8d9b315305afc9523d56b60fc628874f63bf43
Javlon1221 <avazbekzalilov835@gmail.com>
2025-05-12 08:44:58 +0500

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Header from './components/header/header';
-
+import Header from './components/Header/Header';
 function App() {
   return (
     <div className="App">
-      <HEADER/>
+      <Header />
     </div>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,27 @@
+
+import './../Header/Header.css'
+
 const Header = () => {
     return (
         <header>
-            <h2>header</h2>
+            <div className="san">
+                <div className="container">
+                    <nav className="navbar">    
+                        <p className="logo">Escape.</p>
+                        <ul className="nabvar_ul">
+                            <li><a href="#" className="navbar_link">Home</a></li>
+                            <li><a href="#" className="navbar_link">Categories</a></li>
+                            <li><a href="#" className="navbar_link">About</a></li>
+                            <li><a href="#" className="navbar_link">Contact</a></li>
+                        </ul>
+                    </nav>
+                    <div className="hero">
+                        <h1 className="hero_text">Let's do it together.</h1>
+                        <p className='hero_chan'>We travel the world in search of stories. Come along for the ride.</p>
+                        <button className='hero_btn'>View Latest Posts</button>
+                    </div>
+                </div>
+            </div>
         </header>
     )
 }
